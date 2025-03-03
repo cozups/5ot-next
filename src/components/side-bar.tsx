8b5735f4570d426351b7000086e798788666b828
Miso Kim <kimms9725@naver.com>
2025-03-03 20:16:54 +0900

    refactor: ssr side bar

--- src/components/side-bar.tsx
+++ src/components/side-bar.tsx
@@ -1,50 +1,20 @@
-'use client';
-import { useEffect, useState } from 'react';
-import clsx from 'clsx';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 
 import { Category } from '@/types/category';
-import { createClient } from '@/utils/supabase/client';
+import { createClient } from '@/utils/supabase/server';
+import SideBarWrapper from './side-bar-wrapper';
 
-export default function SideBar() {
-  const path = usePathname();
-  const [categories, setCategories] = useState<Category[]>([]);
+export default async function SideBar() {
+  const supabase = await createClient();
+  const { data } = await supabase.from('category').select();
 
-  useEffect(() => {
-    async function getCategories() {
-      const supabase = createClient();
-
-      try {
-        const { data } = await supabase.from('category').select();
-
-        setCategories(data as Category[]);
-      } catch (error) {
-        if (error instanceof Error) {
-          console.log(error.message);
-        } else {
-          console.log('알 수 없는 에러:', error);
-        }
-      }
-    }
-
-    getCategories();
-  });
   return (
-    <div
-      className={clsx(
-        'w-48 ml-4 pl-8 py-8',
-        `${
-          path === '/' ? 'absolute bg-black/30 text-white' : 'fixed'
-        } top-1/2 -translate-y-1/2`,
-        'flex flex-col gap-4'
-      )}
-    >
+    <SideBarWrapper>
       <div>
         <h2 className="text-2xl my-1 font-semibold">Men</h2>
         <ul>
-          {categories
-            .filter((category) => category.sex === 'men')
+          {(data as Category[])
+            ?.filter((category) => category.sex === 'men')
             .map((menu) => (
               <li key={menu.id}>
                 <Link href={`/${menu.sex}/${menu.name}`}>{menu.name}</Link>
@@ -55,7 +25,7 @@ export default function SideBar() {
       <div>
         <h2 className="text-2xl my-1 font-semibold">Woman</h2>
         <ul>
-          {categories
+          {(data as Category[])
             .filter((category) => category.sex === 'women')
             .map((menu) => (
               <li key={menu.id}>
@@ -64,6 +34,6 @@ export default function SideBar() {
             ))}
         </ul>
       </div>
-    </div>
+    </SideBarWrapper>
   );
 }
