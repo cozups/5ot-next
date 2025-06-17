37621602798245ddad7b73047bc2613c7cd1c532
Miso Kim <kimms9725@naver.com>
2025-06-17 23:58:07 +0900

    feat: 메인 페이지 new 연동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,31 @@
 import MainImageSlider from '@/components/main-image-slider';
 import SideBar from '@/components/side-bar';
+import { createClient } from '@/utils/supabase/server';
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createClient();
+  const { data } = await supabase.from('clothes').select().limit(4);
+
   return (
     <div className="w-full h-full grid grid-cols-[1fr_4fr_1fr]">
       <SideBar />
       <div className="w-full">
         <MainImageSlider />
-        <div>
-          <h2 className="my-4 text-2xl font-bold">new</h2>
-          <div className="grid grid-cols-4 gap-4">
-            {Array.from({ length: 4 }).map((_, i) => (
-              <div key={i} className="bg-violet-500 w-full h-56 rounded-lg">
-                ,
-              </div>
-            ))}
+        {data && data.length > 0 && (
+          <div>
+            <h2 className="my-4 text-2xl font-bold">new</h2>
+            <div className="grid grid-cols-4 gap-4">
+              {data.map((clothes) => (
+                <div
+                  key={clothes.id}
+                  className="bg-violet-500 w-full h-56 rounded-lg"
+                >
+                  {clothes.name}
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
