6567abdfa04e53d2b60644a025be25b50c8389a3
Miso Kim <kimms9725@naver.com>
2025-06-18 18:54:59 +0900

    feat: 메인 페이지 new 상품 렌더링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import MainImageSlider from '@/components/main-image-slider';
 import SideBar from '@/components/side-bar';
+import { Clothes } from '@/types/clothes';
 import { createClient } from '@/utils/supabase/server';
+import clsx from 'clsx';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default async function Home() {
   const supabase = await createClient();
-  const { data } = await supabase.from('clothes').select().limit(4);
+  const { data } = await supabase
+    .from('clothes')
+    .select()
+    .order('created_at', { ascending: false })
+    .limit(4);
 
   return (
     <div className="w-full h-full min-h-[calc(100vh-10rem)] grid grid-cols-[1fr_4fr_1fr]">
@@ -15,12 +23,30 @@ export default async function Home() {
           <div>
             <h2 className="my-4 text-2xl font-bold">new</h2>
             <div className="grid grid-cols-4 gap-4">
-              {data.map((clothes) => (
+              {data.map((clothes: Clothes) => (
                 <div
                   key={clothes.id}
-                  className="bg-violet-500 w-full h-56 rounded-lg"
+                  className="w-full h-56 rounded-lg relative overflow-hidden group"
                 >
-                  {clothes.name}
+                  <Image
+                    src={clothes.image}
+                    alt={clothes.name}
+                    fill
+                    className="object-cover"
+                  />
+
+                  {/* 이 div가 안 보였다가 커서가 올라오면 서서히 나타나게 하고 싶은데 어떻게 하는거지 */}
+                  <Link href={`/${clothes.category}/${clothes.id}`}>
+                    <div
+                      className={clsx(
+                        'bg-[rgba(0,0,0,0.25)] absolute top-0 left-0 w-full h-full cursor-pointer',
+                        'flex items-center justify-center',
+                        'opacity-0 group-hover:opacity-100 transition-opacity duration-300'
+                      )}
+                    >
+                      <p className="text-white font-semibold">{clothes.name}</p>
+                    </div>
+                  </Link>
                 </div>
               ))}
             </div>
