3da94993a7d5517a8e5c4324f76bd428413f67ef
Miso Kim <kimms9725@naver.com>
2025-08-05 17:24:50 +0900

    feat: 1MB 이하 안내 문구 추가

--- src/components/product/product-form.tsx
+++ src/components/product/product-form.tsx
@@ -1,37 +1,31 @@
-'use client';
+"use client";
 
-import { useActionState, useEffect, useRef, useState } from 'react';
-import { Button } from '../ui/button';
-import { Input } from '../ui/input';
-import { insertProduct, ProductFormState } from '@/actions/products';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '../ui/select';
-import { createClient } from '@/utils/supabase/client';
-import { Category } from '@/types/category';
-import Image from 'next/image';
-import { toast } from 'sonner';
-import { useInvalidateCache } from '@/hooks/useInvalidateCache';
+import { useActionState, useEffect, useRef, useState } from "react";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { insertProduct, ProductFormState } from "@/actions/products";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
+import { createClient } from "@/utils/supabase/client";
+import { Category } from "@/types/category";
+import Image from "next/image";
+import { toast } from "sonner";
+import { useInvalidateCache } from "@/hooks/useInvalidateCache";
 
 const initialState: ProductFormState = { success: false };
 
 export default function ProductForm() {
   const [formState, formAction] = useActionState(insertProduct, initialState);
-  const [sex, setSex] = useState<string>('men');
+  const [sex, setSex] = useState<string>("men");
   const [category, setCategory] = useState<Category[]>([]);
   const [pickedImage, setPickedImage] = useState<string | null>(null);
   const formRef = useRef<HTMLFormElement>(null);
 
-  const { invalidateCache } = useInvalidateCache(['products']);
+  const { invalidateCache } = useInvalidateCache(["products"]);
 
   useEffect(() => {
     const getCategory = async () => {
       const supabase = createClient();
-      const { data } = await supabase.from('category').select().eq('sex', sex);
+      const { data } = await supabase.from("category").select().eq("sex", sex);
 
       if (data) {
         setCategory(data);
@@ -45,18 +39,18 @@ export default function ProductForm() {
     if (formState.success) {
       formRef.current?.reset();
       setPickedImage(null);
-      toast.success('제품이 추가되었습니다.');
+      toast.success("제품이 추가되었습니다.");
       invalidateCache();
     }
 
     if (formState.errors?.insertError) {
-      toast.error('제품 추가 중 문제가 발생하였습니다.', {
+      toast.error("제품 추가 중 문제가 발생하였습니다.", {
         description: formState.errors.insertError[0],
       });
     }
 
     if (formState.errors?.imageUpload) {
-      toast.error('제품 이미지 업로드 중 문제가 발생하였습니다.', {
+      toast.error("제품 이미지 업로드 중 문제가 발생하였습니다.", {
         description: formState.errors.imageUpload[0],
       });
     }
@@ -83,11 +77,7 @@ export default function ProductForm() {
   };
 
   return (
-    <form
-      action={formAction}
-      className="bg-blue-50 px-6 py-4 rounded-2xl my-4"
-      ref={formRef}
-    >
+    <form action={formAction} className="bg-blue-50 px-6 py-4 rounded-2xl my-4" ref={formRef}>
       <div className="w-full flex items-center gap-8 mb-4">
         <div className="w-1/2 flex flex-col gap-4">
           <div>
@@ -133,11 +123,7 @@ export default function ProductForm() {
               defaultValue={formState?.values?.description}
               className="w-[80%] bg-white"
             />
-            {
-              <p className="text-red-500 text-sm">
-                {formState?.errors?.description}
-              </p>
-            }
+            {<p className="text-red-500 text-sm">{formState?.errors?.description}</p>}
           </div>
           <div className="flex items-center gap-4">
             <div>
@@ -167,34 +153,18 @@ export default function ProductForm() {
                   ))}
                 </SelectContent>
               </Select>
-              {
-                <p className="text-red-500 text-sm">
-                  {formState?.errors?.category}
-                </p>
-              }
+              {<p className="text-red-500 text-sm">{formState?.errors?.category}</p>}
             </div>
           </div>
           <div>
             <label htmlFor="image">제품 이미지</label>
-            <Input
-              type="file"
-              name="image"
-              className="w-fit bg-white"
-              id="image"
-              onChange={onChangeImage}
-            />
+            <Input type="file" name="image" className="w-fit bg-white" id="image" onChange={onChangeImage} />
+            <p className="text-xs text-gray-500 mt-1">1MB 이하의 이미지만 삽입해주세요.</p>
             {<p className="text-red-500 text-sm">{formState?.errors?.image}</p>}
           </div>
         </div>
         <div className="flex-1 aspect-square bg-slate-500 relative">
-          {pickedImage && (
-            <Image
-              src={pickedImage}
-              fill
-              alt="product image"
-              className="object-cover"
-            />
-          )}
+          {pickedImage && <Image src={pickedImage} fill alt="product image" className="object-cover" />}
         </div>
       </div>
       <Button>추가</Button>
