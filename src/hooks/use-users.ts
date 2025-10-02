eec9c49734c108b576d958d0465aac7206cee42b
Miso Kim <kimms9725@naver.com>
2025-10-02 16:17:11 +0900

    fix: useUser 에러 핸들링 추가

--- src/hooks/use-users.ts
+++ src/hooks/use-users.ts
@@ -3,7 +3,10 @@ import { useContext } from "react";
 
 export function useUser() {
   const context = useContext(UserContext);
-  // Note: context can be null if a component is not wrapped in UserProvider.
-  // You might want to add an error check here in a real application.
+
+  if (context === null) {
+    throw new Error("useUser를 사용하려면 UserProvider로 감싸야 합니다.");
+  }
+
   return context;
 }
