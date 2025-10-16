aa6ae817f73d825f3b3b966e34c11bccf972911f
Miso Kim <kimms9725@naver.com>
2025-10-16 21:41:21 +0900

    fix: order status 버그 수정

--- src/components/order/order-status-action.tsx
+++ src/components/order/order-status-action.tsx
@@ -1,15 +1,10 @@
-'use client';
+"use client";
 
-import { OrderFormState } from '@/actions/orders';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
-import { toastError } from '@/lib/utils';
-import { toast } from 'sonner';
+import { OrderFormState } from "@/actions/orders";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { useInvalidateCache } from "@/hooks/useInvalidateCache";
+import { toastError } from "@/lib/utils";
+import { toast } from "sonner";
 
 export default function OrderStatusAction({
   defaultValue,
@@ -18,15 +13,18 @@ export default function OrderStatusAction({
   defaultValue: string;
   action: (id: string) => Promise<OrderFormState>;
 }) {
+  const { invalidateCache } = useInvalidateCache(["orders", "admin"]);
+
   const onChangeStatus = async (value: string) => {
     const result = await action(value);
 
     if (result.success) {
-      toast.success('상태가 업데이트 되었습니다.');
+      invalidateCache();
+      toast.success("상태가 업데이트 되었습니다.");
     }
 
     if (result.errors) {
-      toastError('상태 업데이트 중 문제가 발생했습니다.', result.errors);
+      toastError("상태 업데이트 중 문제가 발생했습니다.", result.errors);
     }
   };
 
